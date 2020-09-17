55b2af7fb4c4c63462b3aaf50235947765a7abcd
BayernMuller <cjjunk29@gmail.com>
2020-09-17 11:17:17 +0900

    FeatureMatching - Find Monariza

--- FeatureMatching/BasicExample/find_monariza.py
+++ FeatureMatching/BasicExample/find_monariza.py
@@ -1,8 +1,8 @@
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
-img1 = cv2.imread('./data/logo.png', 0)
-img2 = cv2.imread('./data/allianz.jpg', 0)
+img1 = cv2.imread('./data/monariza.jpg', 0)
+img2 = cv2.imread('./data/museum.jpg', 0)
 
 sift = cv2.xfeatures2d.SIFT_create()
 kp1, des1 = sift.detectAndCompute(img1,None)
@@ -11,7 +11,8 @@ bf = cv2.BFMatcher()
 matches = bf.knnMatch(des1,des2, k=2)
 good = []
 for m,n in matches:
-    if m.distance < 0.3*n.distance:
+    if m.distance < 0.7 * n.distance:
         good.append([m])
+
 img3 = cv2.drawMatchesKnn(img1,kp1,img2,kp2,good,None,flags=2)
 plt.imshow(img3),plt.show()
